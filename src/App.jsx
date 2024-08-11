a5401d6a2555450dc9c2e0cca5310b0dd47d2c3d
Mekhrona <mehronatursunboeva0@gmail.com>
2024-08-11 11:38:48 +0500

    add .

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import RouteController from './routes/RouteController'
+
+function App() {
+ 
+
+  return (
+    <>
+      <RouteController/>
+    </>
+  )
+}
+
+export default App
